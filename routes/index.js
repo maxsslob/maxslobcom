c776294ade018eff40954e473090b303c651387f
maxsslob <maxsslob@gmail.com>
2016-09-04 22:01:28 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+/* GET work page. */
+router.get('/work', function(req, res, next) {
+  res.render('work');
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+module.exports = router;
